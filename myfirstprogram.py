5225f480517efd7c3bb804c2b85a78dbf3e88f12
Divyam Mahajan <divyammahajan8@gmail.com>
2020-11-10 03:32:49 +0530

    Deleted the last line of the code

--- myfirstprogram.py
+++ myfirstprogram.py
@@ -4,4 +4,4 @@ print ('My age is :')
 myage= input()
 name= 'divyam'
 print ('I will be ' + str(int(myage) + 1) + 'years old by next year')
-print ('My name consists of:' + str(len(myname)) + 'characters')
+
