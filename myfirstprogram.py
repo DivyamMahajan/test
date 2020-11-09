4032b6f94f829420029efea3bdb0b0c47be7116a
Divyam Mahajan <divyammahajan8@gmail.com>
2020-11-10 03:01:53 +0530

    my first file to be uploaded

---
+++ myfirstprogram.py
@@ -0,0 +1,7 @@
+print (' My name is : ')
+myname = input()
+print ('My age is :')
+myage= input()
+name= 'divyam'
+print ('I will be ' + str(int(myage) + 1) + 'years old by next year')
+print ('My name consists of:' + str(len(myname)) + 'characters')
